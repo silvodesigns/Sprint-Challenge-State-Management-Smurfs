292a264a79143413fb2b795a056eeeacfee2f592
silvodesigns <kevsilvo@gmail.com>
2019-10-27 19:28:04 -0400

    created reducer mock

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import {createStore, applyMiddleware} from 'redux'
+import thunk from 'react-thunk';
+import logger from 'redux-logger';
+import {Provider} from 'react-redux';
 
 
 import "./index.css";
@@ -9,4 +12,4 @@ import App from "./components/App";
 
 const store = createStore(reducer, applyMiddleware(thunk, logger))
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
