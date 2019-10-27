6a08f431577598716bb62f4d44feccf02260c214
silvodesigns <kevsilvo@gmail.com>
2019-10-27 19:15:25 -0400

    created store and middleware

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import {createStore, applyMiddleware} from 'redux'
+
+
 import "./index.css";
 import App from "./components/App";
+
+const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(<App />, document.getElementById("root"));
